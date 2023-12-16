fef3ef0265fd7fbabb5153f6a532671c68e928ee
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2023-12-16 18:46:14 +0530

    mongo db data create done

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-import express  from "express";
+import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
 
-const app =express();
+const app = express();
+// database connection with mongo db
+mongoose.connect("mongodb://localhost:27017", {
+    dbName: "backend",
+}).then(() => console.log("dataBase connected")).catch((e) => console.log(e));
 
-const details=[];
+const messageSchema= new mongoose.Schema({
+    name:String,
+    email:String
+});
+
+const msg=mongoose.model("Message",messageSchema)
+
+
+// const details = [];
 
 //using middleware
-app.use(express.static(path.join(path.resolve(),"public")));
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }))
 
-app.set("view engine","ejs");          //setting up view engine for rending without anychange
+app.set("view engine", "ejs");          //setting up view engine for rending without anychange
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     // res.send('hi');
     // res.status(404).send("not found");
 
@@ -26,29 +39,38 @@ app.get("/",(req,res)=>{
 
 
     // res.sendFile(path.join(currentpath,"./home.html")); //for adding directory
-    
 
-    res.render("index",{name:"avdesh"});                    //here they render code with only name 
+
+    res.render("index", { name: "avdesh" });                    //here they render code with only name 
     // res.sendFile("index");
 });
 
-app.get("/success",(req,res)=>{
+app.get("/success", (req, res) => {
     res.render("success");
 })
 
-app.get("/users",(req,res)=>{
-    res.json({details});
+app.get("/add", async (req, res) => {
+    await msg.create({name:"avde",email:"abcaaaa@gmail.com"}).then(()=>{
+        res.send("nice")
+    }
+    )
+    
+
 })
 
-app.post("/contact",(req,res)=>{
+app.get("/users", (req, res) => {
+    res.json({ details });
+})
+
+app.post("/contact", async (req, res) => {
     console.log(req.body.name);
 
-    details.push({username:req.body.name,email:req.body.email});
+    const data={ name: req.body.name, email: req.body.email };
     // res.render("sucess");  //render success file in videw folder after submit
-    
+    await msg.create(data);
     res.redirect("/success");
 })
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("server is running")
 });
